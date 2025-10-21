badc205cdc0e71d65e69a4c561e700de2819e772
al3ksandraa <dermeleva07@mail.ru>
2025-10-21 13:53:05 +0300

    wrote layout swap from rus to eng

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,74 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const layout_map: { [key: string]: string } = {
+    'й': 'q', 'ц': 'w', 'у': 'e', 'к': 'r', 'е': 't', 'н': 'y', 'г': 'u', 'ш': 'i', 'щ': 'o', 'з': 'p',
+    'ф': 'a', 'ы': 's', 'в': 'd', 'а': 'f', 'п': 'g', 'р': 'h', 'о': 'j', 'л': 'k', 'д': 'l',
+    'я': 'z', 'ч': 'x', 'с': 'c', 'м': 'v', 'и': 'b', 'т': 'n', 'ь': 'm',
+    
+    'х': '[', 'ъ': ']',
+    'ё': '`',
+    'э': "'",
+    
+    '"': '@',
+    '№': '#',
+    ';': '$',
+    ':': '^',
+    '?': '&',
+    
+    'ж': ';',
+    'б': ',',
+    'ю': '.',
+    '.': '/',
+    'Б': '<', 
+    'Ю': '>'  
+};
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension layoutswap is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "layoutswap" is now active!');
+	const disposable = vscode.commands.registerCommand('layoutswap.Swap', () => {
+		const editor = vscode.window.activeTextEditor; // Активный файл
+		if (!editor){
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('layoutswap.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LayoutSwap!');
+		// Выделенный текст
+		const selection = editor.selection;
+		const text = editor.document.getText(selection);
+		if (!text){
+			vscode.window.showWarningMessage("Нет выделенного текста")
+		}
+
+		const changed_text = SwapLayout(text);
+		
+		editor.edit(editBuilder => {
+			editBuilder.replace(selection, changed_text);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function SwapLayout(text: string): string {
+	let res = '';
+
+	for (let i = 0; i < text.length; i++){
+		const char = text[i];
+		const lower_char = char.toLowerCase();
+		
+		let new_char = layout_map[lower_char];
+		if (layout_map[char]){
+			new_char = layout_map[char];
+		}
+
+		if (new_char){ // Если буква есть в мапе
+			res += (char == lower_char) ? new_char : new_char.toUpperCase();
+		} else {
+			res += char;
+		}
+	}
+
+	return res;
+}
+
 export function deactivate() {}
